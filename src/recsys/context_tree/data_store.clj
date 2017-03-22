31b62ddb3b8d3e40f09cb7cdfe90af37b2163208
Herve Schnegg <herve.schnegg@telegraph.co.uk>
2017-03-22 22:22:24 +0000

    Packaged query retrieving matching children into a function

--- src/recsys/context_tree/data_store.clj
+++ src/recsys/context_tree/data_store.clj
@@ -26,13 +26,21 @@
                            (hb/put ct parent :value [:child page-kw new-value])))))))
 
 
-(into {}
-      (map (fn [s]
-             (hash-map
-              (hb-util/as-str (.getRow s))
-              (hb/latest-as-map s
-                                :map-family #(keyword (hb-util/as-str %))
-                                :map-qualifier #(keyword (hb-util/as-str %))
-                                :map-value #(hb-util/as-str %) str)))
-           (hb/with-scanner [ct (hb/table "context-tree")]
-             (hb/scan ct :start-row "a"))))
+(defn retrieve-matching-journeys [last-page]
+  "Retrieve all journeys with the same last page"
+  (into {}
+        (map (fn [scanner]
+               (hash-map
+                ;; retrieve keys (parents)
+                (hb-util/as-str (.getRow scanner))
+                ;; retrieve children details
+                (hb/latest-as-map scanner
+                                  :map-family #(keyword (hb-util/as-str %))
+                                  :map-qualifier #(keyword (hb-util/as-str %))
+                                  :map-value #(hb-util/as-str %) str)))
+             (hb/with-scanner [ct (hb/table "context-tree")]
+               (hb/scan ct :start-row last-page :stop-row (str last-page "zzz"))))))
+  
+  
+
+
