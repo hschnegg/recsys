088f334d1ca795bd84b71d08d8e7e2082fc8909d
Herve Schnegg <herve.schnegg@telegraph.co.uk>
2017-03-26 21:10:49 +0100

    Extract reversed parent and page from delimited journey in string

--- src/recsys/context_tree/core.clj
+++ src/recsys/context_tree/core.clj
@@ -1,5 +1,21 @@
-(ns recsys.context-tree.core
+(ns ^{:doc "Implement the main features of the Context Tree recommender"
+      :author "Herve Schnegg"}
+
+    recsys.context-tree.core
+
   (:require [config.core :refer [env]]
-            [recsys.context-tree.data-store :as ds]))
+            [recsys.context-tree.data-store :as ds]
+            [clojure.string :as str]))
 
 
+(defn process-journey [journey]
+  "Break a string containing a journey into a map of page and reversed parent"
+  (let [pages (str/split journey (re-pattern (str "\\" (:journey-separator (:context-tree env)))))]
+    (-> pages
+        (#(if (:hash-page-name? (:context-tree env))
+            (map hash %1)
+            %1))
+        (reverse)
+        (#(hash-map
+           :page (first %1)
+           :parent (str/join (:journey-separator (:context-tree env)) (rest %1)))))))
