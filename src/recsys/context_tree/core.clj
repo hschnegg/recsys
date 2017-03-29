57d9760e67108b07bee872233ca21200ce9d8294
hschnegg <hschnegg@gmail.com>
2017-03-29 23:47:20 +0100

    Improved journey add and retrieve functions

--- src/recsys/context_tree/core.clj
+++ src/recsys/context_tree/core.clj
@@ -8,30 +8,39 @@
             [clojure.string :as str]))
 
 
-(defn process-journey [journey]
-  "Break a string containing a journey into a map of page and reversed parent"
-  (let [pages (str/split journey (re-pattern (str "\\" (:journey-separator (:context-tree env)))))]
-    (-> pages
-        (#(if (:hash-page-name? (:context-tree env))
-            (map hash %1) ;; TODO: should no hash last page
-            %1))
-        (reverse)
-        (#(hash-map
-           :page (first %1)
-           :parent (str/join (:journey-separator (:context-tree env)) (rest %1)))))))
+(defn string-to-vector [journey-string]
+  "Transform a journey defined as a separated string into a vector of its pages"
+  (str/split journey-string (re-pattern (str "\\" (:journey-separator (:context-tree env))))))
 
 
-(defn add-journey [journey]
+(defn vector-to-string [journey-vector]
+  "Transform a journey stored as a vector of pages into a separated string"
+  (str/join (:journey-separator (:context-tree env)) journey-vector))
+
+
+(defn process-parent [parent-journey-vector]
+  "Reverse the journey and hash its elements if required"
+  (let [reversed-parent (reverse parent-journey-vector)]
+    (if (:hash-page-name? (:context-tree env))
+      (map hash reversed-parent)
+      (identity reversed-parent))))
+
+
+(defn add-journey [journey-string]
   "Add a journey stored in a string into the CT data store"
-  (let [processed-journey (process-journey journey)
-        page (:page processed-journey) ;; TODO: should not be hashed
-        parent (:parent processed-journey)]
-    (when (not= parent "")
-                (ds/store-journey parent page))))
+  (let [journey-vector (string-to-vector journey-string)
+        page (last journey-vector)
+        parent-vector (process-parent (pop journey-vector))]
+    (when (> (count parent-vector) 0)
+      (ds/store-journey (vector-to-string parent-vector) page))))
 
 
-(defn get-parent-journeys [journey]
+(defn retrieve-journey-descendants [journey-string]
   "Retrieve all journeys with the same last page"
-  (let [processed-journey (process-journey journey)
-        page (:page processed-journey)] ;; TODO: hashed
-    (ds/retrieve-matching-journeys page)))
+  (let [journey-vector (string-to-vector journey-string)
+        last-page (last journey-vector)]    
+    (ds/retrieve-matching-journeys last-page)))
+
+
+
+
