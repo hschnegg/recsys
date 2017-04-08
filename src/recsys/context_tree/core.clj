55369cebb6e3d87501966863e9ca2011a1f87629
hschnegg <hschnegg@gmail.com>
2017-04-08 22:30:25 +0100

    Improved weighted recs

--- src/recsys/context_tree/core.clj
+++ src/recsys/context_tree/core.clj
@@ -42,7 +42,7 @@
     (ds/retrieve-matching-journeys last-page)))
 
 
-(defn retrieve-journey-family [last-page]
+(defn retrieve-subtree [last-page]
   "Retrieve all journeys with the same last page"
     (ds/retrieve-matching-journeys last-page))
 
@@ -62,52 +62,70 @@
   ([reversed-journey-vector suffixes]
    (if (empty? reversed-journey-vector)
      suffixes
-     (recur (pop reversed-journey-vector) (conj suffixes reversed-journey-vector)))))        
+     (recur (drop-last reversed-journey-vector) (conj suffixes (vector-to-string reversed-journey-vector))))))        
 
 
-(def family (retrieve-journey-family "c"))
+(def subtree (retrieve-subtree "c"))
 (def suffixes (map vector-to-string (build-all-suffixes ["c" "b" "a"])))
 (def weights (suffix-weights (count suffixes)))
 (def weighted-suffixes (zipmap suffixes weights))
-(def family-parents (keys family))
+(def subtree-parents (keys subtree))
 
-(defn get-matching-journeys [suffix family-parents]
-  (filter (fn [parent] (str/starts-with? parent suffix)) family-parents))
+(defn get-matching-journeys [suffix subtree-parents]
+  (filter (fn [parent] (str/starts-with? parent suffix)) subtree-parents))
 
-(def matching-parents (get-matching-journeys "c|b" family-parents))
+(def matching-parents (get-matching-journeys "c|b" subtree-parents))
 
 
 (defn abs-to-perc [vector]
   (map #(/ % (reduce + vector)) vector))
 
 
-(defn get-pages-visits [parent weight tree]
+(defn get-pages-visits [parent tree]
   (let [pages-kw (keys (:child (get tree parent)))
         pages (map name pages-kw)
         visits-str (vals (:child (get tree parent)))
-        visits (map #(* % weight) (abs-to-perc (map #(Integer. %) visits-str)))]
+        ;; visits (map #(* % weight) (abs-to-perc (map #(Integer. %) visits-str)))]
+        visits (map #(Integer. %) visits-str)]
     (zipmap pages visits)))
         
 
 (defn get-suffix-children [suffix weight tree]
-  (->> [suffix]
-       ;; extract matching journeys
-       (mapcat #(get-matching-journeys % (keys tree)))
-       ;;(fn [s] (filter (fn [parent] (str/starts-with? parent [s])) tree))
-       (distinct)
-       (sort-by count)
-       (reverse)
-       ;; retrieve children and visits
-       (map #(get-pages-visits % weight tree))
-       ;; aggregate resulting hashmap
-       (apply merge-with +)))
+  (let [children (->> [suffix]
+                      ;; extract matching journeys
+                      (mapcat #(get-matching-journeys % (keys tree)))
+                      ;;(fn [s] (filter (fn [parent] (str/starts-with? parent [s])) tree))
+                      (distinct)
+                      (sort-by count)
+                      (reverse)
+                      ;; retrieve children and visits
+                      (map #(get-pages-visits % tree))
+                      ;; aggregate resulting hashmap
+                      (apply merge-with +))
+        pages (keys children)
+        visits (map #(* % weight) (abs-to-perc (vals children)))]
+    (zipmap pages visits)))
 
 
-(defn iterate-over-suffixes [weighted-suffixes tree]
-  (let [first-weighted-suffix (first weighted-suffixes)
-        first-suffix (first first-weighted-suffix)
-        weight (last first-weighted-suffix)]
-    (when first-suffix
-      (prn first-suffix)
-      (prn (get-suffix-children first-suffix weight tree))
-      (recur (rest weighted-suffixes) (dissoc tree first-suffix)))))
+(defn iterate-over-suffixes
+  ([weighted-suffixes tree]
+   (iterate-over-suffixes weighted-suffixes tree {}))
+  ([weighted-suffixes tree recs]
+   (let [first-weighted-suffix (first weighted-suffixes)
+         first-suffix (first first-weighted-suffix)
+         weight (last first-weighted-suffix)]
+     (if (empty? first-suffix)
+       (reverse (sort-by val recs))
+       (recur (rest weighted-suffixes) (dissoc tree first-suffix) (merge-with + recs (get-suffix-children first-suffix weight tree)))))))
+
+
+
+(defn retrieve-recommendations [journey-string]
+  (let [journey-vector (string-to-vector journey-string)
+        reversed-journey-vector (reverse journey-vector)
+        last-page (first reversed-journey-vector)
+        subtree (retrieve-subtree last-page)
+        suffixes (build-all-suffixes reversed-journey-vector)
+        weights (suffix-weights (count suffixes))]
+    (prn suffixes)
+    (iterate-over-suffixes (zipmap suffixes weights) subtree)))
